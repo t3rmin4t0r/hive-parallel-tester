c700608bb4b7001275666261dd4d4ed906b34110
Gopal V <gopalv@php.net>
2013-05-29 14:04:38 +0530

    fix prologue up for hadoop-1 as well

--- prologue.sh
+++ prologue.sh
@@ -1,10 +1,12 @@
 set -e;
+exec &> stdout;
 prologue() {
 export ANT_OPTS='-Xms768m -Xmx2048m -XX:PermSize=128m -XX:MaxPermSize=768m';
-export TESTARGS='-Dtest.junit.output.format=xml -Dtest.output=yes -Dtest.junit.output.usefile=true -Dcheckstyle.failOnViolation=false';
-export PATH=$PATH:$PWD/ant/bin;
-umask 022
-exec &> stdout;
+export TESTARGS='-Dtest.junit.output.format=xml -Dtest.output=yes -Dtest.junit.output.usefile=true -Djavadoc.executable=/bin/true';
+test -d ./ant || (mv ant ant.zip && mkdir ant && cd ant && $JAVA_HOME/bin/jar xf ../ant.zip);
+export PATH=$PATH:$PWD/ant/*/bin;
+umask 022;
+# Redirect stdout ( > ) into a named pipe ( >() ) running "tee"
 mkdir hive; 
 cd hive; 
 tar xf ../hive-build.tar; 
