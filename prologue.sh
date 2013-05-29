7894a48fe456fb6d947878f9bb58c639fba49bf4
Gopal V <gopalv@apache.org>
2013-05-29 06:10:29 -0400

    load ivy and maven from local dirs

--- prologue.sh
+++ prologue.sh
@@ -1,16 +1,16 @@
-set -e;
+set -e -x ;
 exec &> stdout;
 prologue() {
-export ANT_OPTS='-Xms768m -Xmx2048m -XX:PermSize=128m -XX:MaxPermSize=768m';
-export TESTARGS='-Dtest.junit.output.format=xml -Dtest.output=yes -Dtest.junit.output.usefile=true -Djavadoc.executable=/bin/true';
-test -d ./ant || (mv ant ant.zip && mkdir ant && cd ant && $JAVA_HOME/bin/jar xf ../ant.zip);
-export PATH=$PATH:$PWD/ant/*/bin;
+
+export HOME=$PWD;
+export TESTARGS="-Dtest.junit.output.format=xml -Dtest.output=yes -Dtest.junit.output.usefile=true -Djavadoc.executable=/bin/true -Duser.home=$PWD -Divy.default.ivy.user.dir=$PWD/.ivy/ -DDmaven.local.repo=$PWD/.m2";
+export PATH=$PATH:$(ls -d $PWD/ant/*/bin);
 umask 022;
-# Redirect stdout ( > ) into a named pipe ( >() ) running "tee"
 mkdir hive; 
 cd hive; 
 tar xf ../hive-build.tar; 
 find -name TEST*.xml -delete;
 # hcatalog runs checkstyle during test
 sed -i~ "s@<antcall.*checkstyle/>@@" hcatalog/build.xml;
+ant package $TESTARGS;
 }
