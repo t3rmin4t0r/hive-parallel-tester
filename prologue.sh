97becfed6ab8f29e424a2b0a78717592b114dc3b
Gopal V <gopalv@php.net>
2013-05-30 15:32:09 +0530

    re-add the ant-opts & skip the ant package part

--- prologue.sh
+++ prologue.sh
@@ -1,9 +1,9 @@
-set -e -x ;
+set -e ;
 exec &> stdout;
 prologue() {
-
+export ANT_OPTS='-Xms768m -Xmx2048m -XX:PermSize=128m -XX:MaxPermSize=768m';
 export HOME=$PWD;
-export TESTARGS="-Dtest.junit.output.format=xml -Dtest.output=yes -Dtest.junit.output.usefile=true -Djavadoc.executable=/bin/true -Duser.home=$PWD -Divy.default.ivy.user.dir=$PWD/.ivy/ -DDmaven.local.repo=$PWD/.m2";
+export TESTARGS="-Dtest.junit.output.format=xml -Dtest.output=yes -Dtest.junit.output.usefile=true -Djavadoc.executable=/bin/true -Duser.home=$PWD -Divy.default.ivy.user.dir=$PWD/.ivy2/ -DDmaven.local.repo=$PWD/.m2";
 export PATH=$PATH:$(ls -d $PWD/ant/*/bin);
 umask 022;
 mkdir hive; 
@@ -12,5 +12,6 @@ tar xf ../hive-build.tar;
 find -name TEST*.xml -delete;
 # hcatalog runs checkstyle during test
 sed -i~ "s@<antcall.*checkstyle/>@@" hcatalog/build.xml;
-ant package $TESTARGS;
+# if you ship your maven dirs, this is not required
+test -e ../.m2 || ant package $TESTARGS;
 }
