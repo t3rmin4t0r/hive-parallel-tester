71b84b7412c998a4dd76ff9943fa89ad18e69741
Gopal V <gopalv@apache.org>
2013-05-30 08:26:06 -0400

    ship m2 and ivy tars as-is, to prevent conflicts

--- prologue.sh
+++ prologue.sh
@@ -5,15 +5,15 @@ export ANT_OPTS='-Xms768m -Xmx2048m -XX:PermSize=128m -XX:MaxPermSize=768m';
 export HOME=$PWD;
 export TESTARGS="-Dtest.junit.output.format=xml -Dtest.output=yes -Dtest.junit.output.usefile=true -Djavadoc.executable=/bin/true -Duser.home=$PWD -Divy.default.ivy.user.dir=$PWD/.ivy2/ -DDmaven.local.repo=$PWD/.m2";
 export PATH=$PATH:$(ls -d $PWD/ant/*/bin);
+test -e m2.tar && (mkdir .m2; cd .m2; tar -xf ../m2.tar);
+test -e ivy2.tar && (mkdir .ivy2; cd .ivy2; tar -xf ../ivy2.tar);
 umask 022;
-test -d .m2 && chmod 0700 -R .m2;
-test -d .ivy2 && chmod 0700 -R .ivy2;
 mkdir hive; 
 cd hive; 
 tar xf ../hive-build.tar; 
 find -name TEST*.xml -delete;
 # hcatalog runs checkstyle during test
-sed -i~ "s@<antcall.*checkstyle/>@@" hcatalog/build.xml;
+sed -i~ "s@<antcall.*checkstyle.*/>@@" hcatalog/build.xml;
 # if you ship your maven dirs, this is not required
 test -e ../.m2 || ant package $TESTARGS;
 }
